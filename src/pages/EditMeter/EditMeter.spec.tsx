393189e033f2f7e42f80fbe65f8911f2638977db
TiagoDiass <tiago.costadiasss@gmail.com>
2024-02-04 02:09:47 -0300

    test: add tests of EditMeter feature

--- src/pages/EditMeter/EditMeter.spec.tsx
+++ src/pages/EditMeter/EditMeter.spec.tsx
@@ -1,3 +1,67 @@
+import { metersService } from '@/services/meters-service/meters-service';
+import { useMetersStore } from '@/store/use-meters-store';
+import { Meter } from '@/types/meters';
+import {
+	getMeterFormFields,
+	mockPointerEventAndResizeObserver,
+} from '@/utils/test-utils';
+import { render, renderHook, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { BrowserRouter } from 'react-router-dom';
+import { EditMeter } from './EditMeter';
+
+function renderComponent() {
+	vi.spyOn(metersService, 'updateMeter').mockResolvedValue({
+		successfullyUpdated: true,
+	});
+
+	render(
+		<BrowserRouter>
+			<EditMeter />
+		</BrowserRouter>,
+	);
+}
+
+const user = userEvent.setup();
+
+mockPointerEventAndResizeObserver();
+
 describe('Page: EditMeter', () => {
-	it.todo('should edit a meter correctly');
+	it('should edit a meter correctly', async () => {
+		const result = renderHook(() => useMetersStore()).result;
+		const meterMock: Meter = {
+			id: '0839b368-d3d1-41a3-9938-d4c6df8a3d64',
+			api_name: 'STORAGE_USED',
+			display_name: 'Storage Used',
+			active: true,
+			used_for_billing: false,
+			updated_time: '2023-06-07T20:09:22.933Z',
+			created_time: '2023-04-19T00:57:55.628Z',
+			type: 'max',
+		};
+
+		result.current.state.currentMeter = meterMock;
+
+		renderComponent();
+
+		const { displayNameInput, apiNameInput } = getMeterFormFields();
+		const submitButton = screen.getByRole('button', { name: /Update meter/ });
+
+		await user.clear(displayNameInput);
+		await user.type(displayNameInput, 'updated display name');
+
+		await user.clear(apiNameInput);
+		await user.type(apiNameInput, 'NEW_API_NAME');
+
+		await user.click(submitButton);
+
+		expect(metersService.updateMeter).toHaveBeenCalledTimes(1);
+		expect(metersService.updateMeter).toHaveBeenCalledWith(meterMock.id, {
+			display_name: 'updated display name',
+			api_name: 'NEW_API_NAME',
+			type: 'max',
+			active: true,
+			used_for_billing: false,
+		});
+	});
 });
--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,25 @@
-import react from "@vitejs/plugin-react";
-import tsConfigPaths from "vite-tsconfig-paths";
-import { defineConfig } from "vitest/config";
+import react from '@vitejs/plugin-react';
+import tsConfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from 'vitest/config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react(), tsConfigPaths()],
 	test: {
 		globals: true,
-		environment: "jsdom",
-		setupFiles: "./.vitest/vitest-setup.ts",
+		environment: 'jsdom',
+		setupFiles: './.vitest/vitest-setup.ts',
 		coverage: {
-			provider: "v8",
-			exclude: [],
+			provider: 'v8',
+			exclude: [
+				'src/types/*',
+				'src/components/ui/*toast*.{ts,tsx}',
+				'src/main.tsx',
+				'src/pages/index.ts',
+				'src/routes.tsx',
+				'src/vite-env.d.ts',
+				'*.config.js',
+			],
 		},
 	},
 });
