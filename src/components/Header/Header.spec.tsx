95adbe9313753df3d2a11cda10a0c36bec1a2eb7
TiagoDiass <tiago.costadiasss@gmail.com>
2024-02-04 02:13:30 -0300

    test: ensure Header renders correctly

--- src/components/Header/Header.spec.tsx
+++ src/components/Header/Header.spec.tsx
@@ -1,3 +1,31 @@
+import { render } from '@testing-library/react';
+import { Header } from './Header';
+
 describe('Component: Header', () => {
-	it.todo('should render correctly');
+	it('should render correctly', () => {
+		const { container } = render(<Header />);
+
+		expect(container.firstChild).toMatchInlineSnapshot(`
+			<header
+			  class="bg-amber-600 py-4 mb-8 px-6"
+			>
+			  <div
+			    class="w-full max-w-screen-lg mx-auto"
+			  >
+			    <h2
+			      class="text-2xl text-zinc-100"
+			    >
+			      <strong>
+			        Easy
+			      </strong>
+			      <span
+			        class="font-light"
+			      >
+			        Meters
+			      </span>
+			    </h2>
+			  </div>
+			</header>
+		`);
+	});
 });
